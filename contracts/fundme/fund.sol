3c456d06a5cf6cb6f144c99370f3167062960635
SairajMN <sairaj.nadaf@gmail.com>
2025-11-17 22:32:23 +0530

    fund me

--- contracts/fundme/fund.sol
+++ contracts/fundme/fund.sol
@@ -1,9 +1,14 @@
 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18; // 
+pragma solidity ^0.8.18; 
 
-contract FundMe {//
+contract FundMe {
+
+    uint public myVal = 1;
+
     function fund() public payable {
+
+        myVal = myVal + 2;
         require(msg.value > 1e18, "didn't send enough ETH");
     }
 }
